edd3350b4a355f2fd84b107d464e442c3df21a76
sathanxavier1998 <71935696+sathanxavier1998@users.noreply.github.com>
2023-10-07 09:21:54 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -5,7 +5,7 @@ class Config:
     API_HASH = getenv("API_HASH", "3f6835286b03e000ab6d71b37cc35b92")
     BOT_TOKEN = getenv("BOT_TOKEN", "5744492052:AAHwoJ6Xn_jlnvHwVFafYcM0Pqx-TMIVIx4")
     FSUB = getenv("FSUB", "CV_link_ONly")
-    CHID = int(getenv("CHID", "-1000112234"))
+    CHID = int(getenv("CHID", "-1001437493581"))
     SUDO = list(map(int, getenv("SUDO").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Bikash:Bikashop@bikash.cbkkx4c.mongodb.net/?retryWrites=true&w=majority")
     
