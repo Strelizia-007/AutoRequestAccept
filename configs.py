70d901b1c4c0d8ba60a4713d0b68fbb74179311f
Strelizia-007 <143823798+Strelizia-007@users.noreply.github.com>
2023-10-25 16:19:20 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,7 +4,7 @@ class Config:
     API_ID = int(getenv("API_ID", "6620972"))
     API_HASH = getenv("API_HASH", "3f6835286b03e000ab6d71b37cc35b92")
     BOT_TOKEN = getenv("BOT_TOKEN", "6752673741:AAF-SWw9dTRgEB0u3JO3LEASvujstmZwIR8")
-    FSUB = getenv("FSUB", "CV_link_ONly")
+    FSUB = getenv("FSUB", "CV_offical")
     CHID = int(getenv("CHID", "-1002116542152"))
     SUDO = list(map(int, getenv("SUDO", "6047510747 1745047302").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Bikash:Bikashop@bikash.cbkkx4c.mongodb.net/?retryWrites=true&w=majority")
