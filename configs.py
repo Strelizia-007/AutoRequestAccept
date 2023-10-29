c447ab4280ec2ef14ca9584f9aae7a3bcd3df432
Strelizia-007 <143823798+Strelizia-007@users.noreply.github.com>
2023-10-29 22:43:22 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,10 +3,10 @@ from os import path, getenv
 class Config:
     API_ID = getenv("API_ID", '29922662')
     API_HASH = getenv("API_HASH", 'fabd9f89368de7cc31357522a0089a56')
-    BOT_TOKEN = getenv("BOT_TOKEN", '6752673741:AAF6lxcFRxnIj-3cLoSktkfFD1MYCSo_iGA')
-    FSUB = getenv("FSUB", "cvlinkz")
-    CHID = getenv("CHID", '-1001630778710')
-    SUDO = getenv("SUDO", "").split()
+    BOT_TOKEN = getenv("BOT_TOKEN", '6752673741:AAEBQf_ZescNZlF_bp6V6b8b_bpuMXXyr0s')
+    FSUB = getenv("FSUB", "Cv_Offical")
+    CHID = getenv("CHID", '-1001574664407')
+    SUDO = getenv("SUDO", "")
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Bikash:Bikashop@bikash.cbkkx4c.mongodb.net/?retryWrites=true&w=majority")
     
 cfg = Config()
