d47d33f7d08baac7c8fa4e2abdcd85540e66e532
Strelizia-007 <143823798+Strelizia-007@users.noreply.github.com>
2023-10-29 21:53:01 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,7 +6,7 @@ class Config:
     BOT_TOKEN = getenv("BOT_TOKEN", '6752673741:AAF6lxcFRxnIj-3cLoSktkfFD1MYCSo_iGA')
     FSUB = getenv("FSUB", "cvlinkz")
     CHID = getenv("CHID", '-1001630778710')
-    SUDO = getenv("SUDO", "6047510747 1745047302").split()
+    SUDO = getenv("SUDO", "").split()
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Bikash:Bikashop@bikash.cbkkx4c.mongodb.net/?retryWrites=true&w=majority")
     
 cfg = Config()
