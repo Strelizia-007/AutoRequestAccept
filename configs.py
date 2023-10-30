e35a653d60beb2770a1b76ff02d8a0ef49e068cb
Strelizia-007 <143823798+Strelizia-007@users.noreply.github.com>
2023-10-30 20:11:09 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,7 +3,7 @@ from os import path, getenv
 class Config:
     API_ID = int(getenv("API_ID", "29922662"))
     API_HASH = getenv("API_HASH", "fabd9f89368de7cc31357522a0089a56")
-    BOT_TOKEN = getenv("BOT_TOKEN", "6752673741:AAEBQf_ZescNZlF_bp6V6b8b_bpuMXXyr0s")
+    BOT_TOKEN = getenv("BOT_TOKEN", "5652993501:AAEasaLU_rSNl05k9Z9eEgRVtyliEVIGWio")
     FSUB = getenv("FSUB", "cv_offical")
     CHID = int(getenv("CHID", "-1001574664407"))
     SUDO = getenv("SUDO", "6874351976 6047510747").split()
