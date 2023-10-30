18dbfcd257543bef128e79dbcdcb8b767e3d9a46
Strelizia-007 <143823798+Strelizia-007@users.noreply.github.com>
2023-10-30 20:01:25 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,7 +6,7 @@ class Config:
     BOT_TOKEN = getenv("BOT_TOKEN", "6752673741:AAEBQf_ZescNZlF_bp6V6b8b_bpuMXXyr0s")
     FSUB = getenv("FSUB", "cv_offical")
     CHID = int(getenv("CHID", "-1001574664407"))
-    SUDO = getenv("SUDO", "")
+    SUDO = getenv("SUDO", "6874351976 6047510747").split()
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://ironman:ironman@cluster0.o1yjwis.mongodb.net/?retryWrites=true&w=majority")
     
 cfg = Config()
