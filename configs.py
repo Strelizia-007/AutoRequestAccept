222042992a1edde68ff7117e0d09a9e4c5c64809
Strelizia-007 <143823798+Strelizia-007@users.noreply.github.com>
2023-11-03 21:13:47 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,8 +4,8 @@ from os import path, getenv
     API_ID = int(getenv("API_ID", "29922662"))
     API_HASH = getenv("API_HASH", "fabd9f89368de7cc31357522a0089a56")
     BOT_TOKEN = getenv("BOT_TOKEN", "6752673741:AAEBQf_ZescNZlF_bp6V6b8b_bpuMXXyr0s")
-    FSUB = getenv("FSUB", "cv_offical")
-    CHID = int(getenv("CHID", "-1001574664407"))
+    FSUB = getenv("FSUB", "")
+    CHID = int(getenv("CHID", ""))
     SUDO = getenv("SUDO", "6874351976 6047510747").split()
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://ironman:ironman@cluster0.o1yjwis.mongodb.net/?retryWrites=true&w=majority")
     
